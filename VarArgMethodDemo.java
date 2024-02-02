b018f18b5624df92b64c3b31d0ffc695971afa3d
Santosh-Rashinkar <santoshrashinkar0108@gmail.com>
2024-02-02 11:09:01 +0530

    overloading multiple scenario

---
+++ VarArgMethodDemo.java
@@ -0,0 +1,24 @@
+package com.practice.overloading.scenario;
+
+public class VarArgMethodDemo {
+
+	public void m1(String s, int... nums) {
+
+		int res = 0;
+
+		for (int n : nums) {
+
+			res += n;
+		}
+		System.out.println(s);
+		System.out.println(res);
+	}
+
+	public static void main(String[] args) {
+
+		VarArgMethodDemo obj = new VarArgMethodDemo();
+
+		obj.m1("hello", 30, 20, 30);
+	}
+
+}
